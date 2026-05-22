0051e67f10786172310ad4cfc0c328380e01652f
xzmzm <xingzhimeng@gmail.com>
2026-05-23 00:47:02 +0800

    patch-11: allow cross-provider search for custom/localhost providers

--- hermes_cli/model_switch.py
+++ hermes_cli/model_switch.py
@@ -825,7 +825,6 @@ def switch_model(
 
         if (
             target_provider == current_provider
-            and not is_custom
             and not resolved_alias
             and not resolved_in_current_catalog
         ):
