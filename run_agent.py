0d4ae64c81057d14ed5fb2eb7a33e3016f387f23
xzmzm <xingzhimeng@gmail.com>
2026-05-23 00:46:41 +0800

    patch-7: fix _is_ollama_glm_backend() false positive on localhost/proxy endpoints

--- run_agent.py
+++ run_agent.py
@@ -1330,7 +1330,7 @@ class AIAgent:
             return False
         if "ollama" in self._base_url_lower or ":11434" in self._base_url_lower:
             return True
-        return bool(self.base_url and is_local_endpoint(self.base_url))
+        return False
 
     def _should_treat_stop_as_truncated(
         self,
